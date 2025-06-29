8773be8fc3f6995b2fec32d9e9fc4726c8b05197
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-06-30 02:06:36 +0600

    fix: routes to component; add assets to angular.json

--- dashboard-angular/src/app/app.routes.ts
+++ dashboard-angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Homepage } from './homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/',
+  //   pathMatch: 'full',
+  // },
+  { path: 'dashboard', component: Dashboard },
+  { path: '', component: Homepage },
+];
