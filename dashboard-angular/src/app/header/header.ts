2a9824e3f2cb160bec9dc0b4ed6c4b04ab21c5b3
Evgenii Ostapchenko <jena3573467@gmail.com>
2025-07-05 19:44:53 +0600

    feat: new header view

--- dashboard-angular/src/app/header/header.ts
+++ dashboard-angular/src/app/header/header.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  // standalone: true,
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  currentUrl: string = '';
 
+  constructor (private router: Router) { }
+
+  ngOnInit(): void {
+    this.currentUrl = this.router.url;
+  }
+
+  isAdminPage(): boolean {
+    return this.currentUrl === '/admin';
+  }
+
+  isHomePage(): boolean {
+    return this.currentUrl === '/';
+  }
+
+  redirect() {
+    if (this.isAdminPage()) this.router.navigate(['/']);
+    if (this.isHomePage()) this.router.navigate(['/authorization']);
+  }
 }
